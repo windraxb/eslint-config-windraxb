467c079697a55795981348be3278833231e95abc
windraxb <xiaobin_0007@163.com>
2020-07-22 00:16:15 +0800

    refactor: eslint工程重构

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 module.exports = {
-  extends: [
-    './rules/style',
-    './rules/best-practices',
-    './rules/es6.js',
-    './rules/grammer-error.js',
-    './rules/strict.js',
-    './rules/style.js',
-    './rules/variables.js'
-  ].map(require, resolve),
-  parserOptions: {
-    ecmaVersion: '2018',
-    sourceType: 'module',
-  },
-  rules: {}
-}
+    extends: ['./base.js']
+};
